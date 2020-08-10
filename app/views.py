ee02262c5042b7bc7cbe77ade4beb23ff5bd8d5d
Feras Baig <ferasbaig@gmail.com>
2020-08-10 12:22:53 -0500

    adding hotfixes

--- app/views.py
+++ app/views.py
@@ -22,6 +22,9 @@ import jsonify
 import json, urllib
 from django.http import JsonResponse
 
+# todo: import model serializers
+# todo: write out Viewset classes for each component
+
 
 # landing page
 def index(request):
@@ -34,16 +37,9 @@ def modules(request):
 # modules/training (gateway for problem_sets)
 def training(request):
     if request.method == 'GET':
-        # hit studycounts API for each GET request for specific page 
-        full_url = 'https://studycounts.com/api/v1/arithmetic/simple.json'
-        # need to simply pass in a dict to the headers parameter
-        headers = {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGQ3ZjkyZTM4MGNlZDM5YmFmZWE4MmY2NjAwZmUwMGQxNDQ4OTUxNzk5MWVmNGE2NGU1Y2IwOWRkYTJiMWVhNDZhYTI5YjY4NGE2MmVjZjUiLCJpYXQiOjE1OTY5MDk1NTUsIm5iZiI6MTU5NjkwOTU1NSwiZXhwIjoxNjI4NDQ1NTU1LCJzdWIiOiIxMDUyIiwic2NvcGVzIjpbXX0.CpqfyY-6_a9hTDOkoaUyONtnToLuFMEkOyhf800eP_HFUyLOjvHtRNQIJKhurmR4r8l2JsFvZ-ekSE7qAl56HD2KilNYnXQ5Gb7R1fGOiJbD8bf_BckhxDZne7jl4gz-Lf2mQ60JRC8NlXXyMyNGzKT0aAH6ulQ_BmxnBPhjQg9-91HhjQskcvCmkZG3rDs8Qw-26IjaD80QLq4eRn2xW9t7cuNo2fmQOoc_M5XYXKdvfMa1qEZms_Z2OB-EVaMuWVKP8TzmykclhWiTEsUVxdWyXVCSXjbcoN5axTYyWkCfSX3Ob6eULHgoF_ZFWXI-9hAfdoPJBWZfX-lwpCPKklOdDAZgODRIprdYGDEWPqs3eASLfVm1_jhzmbCk0OJ9tH2nio0ankZWkIUy5U85ClsIGPq9wpokHGBwas5mooKqkKpookhAhsLZCSelajSuWk1neX4g6vzXI9vsACHjoj-7FEBqxZw9ev00r2SoknedxlyQsGqlVNxR82bmCBzxTAY1K98TXTFsIzcrB0rEd9R8lx7RjfiMEahX09ANTbp-6xgfTjOzb9pNvqtW35b9zqwcEHW-ZWbHGQxFXkRQmHq76yZnT6bsaWGqT3m6CU7H3xmSwiOw1J1psQuwcqaswfbC0s-cs4M5ilUIn2CMSxyzB_yk467BF1HsCRS0ZoI'
-        }
-        response = requests.get(full_url, headers=headers, data={}).json()
+        # make api request to pandas to access dataframe to get dict for questions, return=1
         
-        # confirm retrieval of JSON from API
-        print(response)
+        # render latex equations from JSON strings that had been converted from mathjax
         
         # store JSON in pandas.dataframe, then store pandas.dataframe in SQLite
 
