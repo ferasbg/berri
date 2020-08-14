d17804b9ca85279ac2c407ef01605863834b5414
Feras Baig <ferasbaig@gmail.com>
2020-08-13 19:28:15 -0500

    merge

---
+++ src/App_BACKUP_1685601.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+import './App.css';
+>>>>>>> POC-1
+=======
+>>>>>>> 57137cd4e8f7ee44678dae7cd7c3029e165f6cf3
+
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import TutorProfile from "./components/TutorProfile"
+import BecomeTutor from "./components/BecomeTutor"
+import MessageBoard from "./components/MessageBoard"
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 57137cd4e8f7ee44678dae7cd7c3029e165f6cf3
+export default class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+            <Switch>
+              <Route path="/" component={Profile} exact/>
+              <Route path="/login" component={Login} exact/>
+              <Route path="/signup" component={Home} exact/>
+              <Route path="/profile" component={Profile} exact/>
+              <Route path="/tutor-profile" component={TutorProfile} exact/>
+              <Route path="/become-tutor" component={BecomeTutor} exact/>
+              <Route path="/messages" component={MessageBoard} exact/>
+            </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+<<<<<<< HEAD
+=======
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+          <Switch>
+            <Route path="/" component={Login} exact/>
+            <Route path="/signup" component={Home} exact/>
+            <Route path="/profile" component={Profile} exact/>
+            <Route path="/tutorProfile" component={TutorProfile} exact/>
+            <Route path="/becomeTutor" component={BecomeTutor} exact/>
+          </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+>>>>>>> POC-1
+=======
+>>>>>>> 57137cd4e8f7ee44678dae7cd7c3029e165f6cf3
---
+++ src/App_REMOTE_1685601.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import TutorProfile from "./components/TutorProfile"
+import BecomeTutor from "./components/BecomeTutor"
+import MessageBoard from "./components/MessageBoard"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+            <Switch>
+              <Route path="/" component={Profile} exact/>
+              <Route path="/login" component={Login} exact/>
+              <Route path="/signup" component={Home} exact/>
+              <Route path="/profile" component={Profile} exact/>
+              <Route path="/tutor-profile" component={TutorProfile} exact/>
+              <Route path="/become-tutor" component={BecomeTutor} exact/>
+              <Route path="/messages" component={MessageBoard} exact/>
+            </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
